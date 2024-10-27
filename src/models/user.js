1ba30cc88e345f1372d7513257728c1d2afdecbe
ramijrock <ramijrock@gmail.com>
2024-10-27 23:30:17 +0530

    User models mobile field type change

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: {type: String ,required: true},
     email: {type: String, require: true},
-    mobile: {type: Number, require: true, unique: true},
+    mobile: {type: String, require: true, unique: true},
     password: {type: String, require: true},
     terms_and_conditions: { type: Boolean, required: true, default: false },
     role: { type: String, required: true, enum: ['customer', 'vendor', 'delivery_boy'], default: 'customer' },
